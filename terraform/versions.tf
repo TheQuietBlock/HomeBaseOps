18ab02a2ae9ccb9b5e38004c74928732be7658e7
Patrick <39878585+TheQuietBlock@users.noreply.github.com>
2025-08-08 19:59:44 +0200

    update

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_version = ">= 1.12.2"
   required_providers {
     proxmox = {
-      source  = "Telmate/proxmox"
+      source = "Telmate/proxmox"
       version = "3.0.2-rc03"
     }
   }
